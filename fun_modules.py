31aa9c2ea2efc4a1b89613fa036b6e8511e08f3d
Faisal <faiz0210@gmail.com>
2026-08-16 00:32:38 +0530

    re-learning python

--- fun_modules.py
+++
@@ -1,4 +0,0 @@
-import sys
-print("Hello")
-sys.exit() # exit and do no execute beyond this
-print("GoodBye")
--- functions.py
+++
@@ -1,5 +0,0 @@
-def plusOne(number):
-    return number + 1
-
-newNumber = plusOne(5)
-print(newNumber)
